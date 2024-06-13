094e41cf3c094b38f604b2b4c0ed82c0c919d50c
rowanbarnes <44321083+rowanbarnes@users.noreply.github.com>
2024-06-13 14:22:03 +1000

    Add utilities

---
+++ util/convert.go
@@ -0,0 +1,19 @@
+package util
+
+// ItoABase26 converts a number to a string containing lowercase letters.
+func ItoABase26(n int) string {
+	if n < 0 {
+		return "invalid"
+	}
+
+	result := ""
+
+	for n > 0 {
+		remainder := n % 26
+		result = string(rune('a'+remainder)) + result
+		n /= 26
+		n--
+	}
+
+	return result
+}
